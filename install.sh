88db2f07ccb4ab2c79868afd2c7b4906090c4ef5
Rodolfo Brizola Toscan <87976680+rbtoscan@users.noreply.github.com>
2023-07-19 22:55:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ docker pull staphb/bowtie2
 docker pull r-base
 
 # creating a custom docker image
-docker build -t task4_image  dockerfiles/r_task4/
+docker build -t task4_image  install/dockerfiles/r_task4/
 
 # copy R script to /bin folder. Replace path accordingly
 chmod +x install/script/task4_genExpress.R
